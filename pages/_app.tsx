0a194b3df75a5246f62ba19229537b262030ffe1
Shashank Neupane <suryaneupane0427@gmail.com>
2025-02-01 14:25:30 +0545

    updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,13 @@ import '../styles/index.scss';
 import '../styles/globals.css';
 import '../styles/prism-dracula.css';
 import type { AppProps } from 'next/app';
-import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
+
+
     </>
   );
 }
