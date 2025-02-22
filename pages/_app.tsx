5359d1ca425f847da548191eb87961fe71172745
Surya Shashank Neupane <suryaneupane0427@gmail.com>
2025-02-22 12:07:47 +0545

    refactor MyApp component to improve Tawk.to script loading logic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,34 +2,27 @@ import '../styles/index.scss';
 import '../styles/globals.css';
 import '../styles/prism-dracula.css';
 import type { AppProps } from 'next/app';
-import {useEffect} from "react"
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     // Ensure script is added only once
-    if (document.getElementById("tawkToScript")) return;
+    if (document.getElementById('tawkToScript')) return;
 
-    const script = document.createElement("script");
-    script.id = "tawkToScript";
+    const script = document.createElement('script');
+    script.id = 'tawkToScript';
     script.async = true;
-    script.src = "https://embed.tawk.to/67b96ab336619b190acb96be/1ikm4hk3c";
-    script.charset = "UTF-8";
-    script.setAttribute("crossorigin", "*");
-    document.body.appendChild(script);
+    script.src = 'https://embed.tawk.to/67b96ab336619b190acb96be/1ikm4hk3c';
+    script.charset = 'UTF-8';
+    script.setAttribute('crossorigin', '*');
 
-    return () => {
-      // Cleanup: Remove script if the component unmounts
-      document.body.removeChild(script);
+    // Ensure the script is added after the window loads
+    window.onload = () => {
+      document.body.appendChild(script);
     };
   }, []);
 
-  return (
-    <>
-      <Component {...pageProps} />
-
-
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
